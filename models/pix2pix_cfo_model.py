Fixed Residual/Complex Only Pix2Pix Model + added masking with thresholded target in Pix2Pix

models/pix2pix_cfo_model.py
@@ -92,11 +92,11 @@ class Pix2PixCFOModel(BaseModel):
         
         complex_opt = deepcopy(opt)
         complex_opt.use_cfg_loss = True
-        self.complex_model = self.netcomplex_model = Pix2PixCFOSubModel(complex_opt)
+        self.complex_model = Pix2PixCFOSubModel(complex_opt)
         self.netcomplex_model_g = self.complex_model.netG
         self.netcomplex_model_d = self.complex_model.netD
 
-        self.fusion_head = self.netfusion_head = self.netfusion_head = FusionHead(input_channels=2)
+        self.fusion_head = self.netfusion_head = FusionHead(input_channels=2)
 
         self.optimizers = [*self.base_model.optimizers, *self.complex_model.optimizers, self.fusion_head.optimizer]
         self.model_names = ['base_model_g', 'base_model_d', 'complex_model_g', 'complex_model_d']  # 'fusion_head'
@@ -204,13 +204,18 @@ class Pix2PixCFOModel(BaseModel):
                     if len(pred.shape) == 4:
                         pred = pred.squeeze(1)
 
-                self.data_idx_train += 1
+                # self.data_idx_train += 1
         else:
             base_pred = self.base_model(self.real_A).unsqueeze(1)
             complex_pred = self.complex_model(self.real_A).unsqueeze(1)
             
             combined = torch.cat([base_x, complex_x], dim=1)
             pred = self.fusion_head(combined)
+            # formular:
+            #   complex = (target - base) * -2
+            #   complex*(-0.5) = target - base
+            #   target = (complex*(-0.5)) + base
+            # pred = (complex_pred*(-0.5)) + base_pred
             if len(pred.shape) == 4:
                 pred = pred.squeeze(1)
 
@@ -276,18 +281,25 @@ class Pix2PixCFOModel(BaseModel):
             # Basline
             pred_ = self.forward_and_return(model_idx=0)
             self.base_model.optimize_parameters(base_data[0], base_data[1], pred_)
+            print(f"\nBaseline Prediction Output:\n    - min = {pred_.min().item()}\n    - max = {pred_.max().item()}\n    - mean = {pred_.mean().item()}\n    - var = {pred_.var().item()}\n    - nan = {torch.isnan(pred_).any()}")
 
             # Complex
             pred_ = self.forward_and_return(model_idx=1)
-            self.base_model.optimize_parameters(complex_data[0], complex_data[1], pred_)
+            self.complex_model.optimize_parameters(complex_data[0], complex_data[1], pred_)
+            print(f"\nComplex Prediction Output:\n    - min = {pred_.min().item()}\n    - max = {pred_.max().item()}\n    - mean = {pred_.mean().item()}\n    - var = {pred_.var().item()}\n    - nan = {torch.isnan(pred_).any()}")
         else:
             # Fusion
             self.fusion_head.optimizer.zero_grad()
             pred_ = self.forward_and_return(model_idx=2)
             self.fusion_head.backward(target_, pred_)
             self.fusion_head.optimizer.step()
+            print(f"\nFusion Prediction Output:\n    - min = {pred_.min().item()}\n    - max = {pred_.max().item()}\n    - mean = {pred_.mean().item()}\n    - var = {pred_.var().item()}\n    - nan = {torch.isnan(pred_).any()}")
 
-        self.data_idx_train += 0
+        self.data_idx_train += 1
+
+        if self.data_idx_train == len(self.datasets[1][0]):
+            self.data_idx_train = 0
+
         self.update_loss()
         # self.adjust_image_shapes()
 

models/pix2pix_model.py
@@ -36,6 +36,7 @@ class Pix2PixModel(BaseModel):
             parser.set_defaults(pool_size=0, gan_mode='vanilla')
             parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
             parser.add_argument('--wgangp', action='store_true', help='Should use WGAN-GP')
+            parser.add_argument('--masked', action='store_true', help='Should mask with the target and threshold at 0')
 
         return parser
 
@@ -46,6 +47,7 @@ class Pix2PixModel(BaseModel):
             opt (Option class)-- stores all the experiment flags; needs to be a subclass of BaseOptions
         """
         BaseModel.__init__(self, opt)
+        self.masked = opt.masked
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
         self.loss_names = ['G_GAN', 'G_L1', 'D_real', 'D_fake']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
@@ -181,7 +183,21 @@ class Pix2PixModel(BaseModel):
             self.loss_G_GAN = self.criterionGAN(pred_fake, True)
         
         # Second, G(A) = B
-        self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B)
+        # L1 loss masked
+        if self.masked:
+            # Compute pixel-wise absolute difference
+            l1_diff = torch.abs(self.fake_B - self.real_B)
+
+            # Create mask where real_B > 0
+            mask = (self.real_B > 0).float() # (self.real_B > 0).astype(np.uint8) * 255
+
+            # Apply mask and normalize
+            masked_l1 = l1_diff * mask
+            num_masked = torch.clamp(mask.sum(), min=1.0)  # prevent division by zero
+            self.loss_G_L1 = masked_l1.sum() / num_masked
+        else:
+            # No masking
+            self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B)
         
         # combine loss and calculate gradients
         self.loss_G = self.loss_G_GAN * self.lambda_GAN + self.loss_G_L1 * self.opt.lambda_L1