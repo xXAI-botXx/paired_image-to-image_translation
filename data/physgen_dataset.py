Fixed bugs with computing reflexions (wrong stepsize/ray amount + wrong values for rays) - added option to draw the rays on the image

data/physgen_dataset.py
@@ -85,6 +85,7 @@ class PhysGenDataset(BaseDataset):
         parser.add_argument('--output_type', type=str, default="standard", help='Decides which output type is used -> standard or complex_only.')
         parser.add_argument('--reflexion_channels', action='store_true', help='Whether to add channels with reflexion traces.')
         parser.add_argument('--reflexion_steps', type=int, default=36, help='Amount of reflexion beams.')
+        parser.add_argument('--reflexions_draw_on_image', action='store_true', help='Whether to add channels on the input image itself.')
         parser.add_argument('--reflexions_as_channels', action='store_true', help='Whether to add channels with reflexion traces.')
         parser.add_argument('--force_reflexion_computation', action='store_true', help='If set, the reflexions will get computed and not loaded.')
         
@@ -96,7 +97,7 @@ class PhysGenDataset(BaseDataset):
 
     def __init__(self, variation="sound_baseline", mode="train", input_type="osm", output_type="standard", 
                  reflexion_channels=False, reflexion_steps=36, reflexions_as_channels=False,
-                 force_reflexion_computation=False):
+                 reflexions_draw_on_image=False, force_reflexion_computation=False):
         """
         Loads PhysGen Dataset.
 
@@ -130,6 +131,7 @@ class PhysGenDataset(BaseDataset):
         
         self.reflexion_channels = reflexion_channels
         self.reflexion_steps = reflexion_steps
+        self.reflexions_draw_on_image = reflexions_draw_on_image
         self.reflexions_as_channels = reflexions_as_channels
         self.force_reflexion_computation = force_reflexion_computation
 
@@ -138,6 +140,7 @@ class PhysGenDataset(BaseDataset):
         print(f"    -> output_type = {output_type}")
         print(f"    -> reflexion_channels = {reflexion_channels}")
         print(f"    -> reflexion_steps = {reflexion_steps}")
+        print(f"    -> reflexions_draw_on_image = {reflexions_draw_on_image}")
         print(f"    -> reflexions_as_channels = {reflexions_as_channels}\n")
 
     def __len__(self):
@@ -184,19 +187,24 @@ class PhysGenDataset(BaseDataset):
             height, width = np.squeeze(input_img.cpu().numpy(), axis=0).shape
             ray_path = os.path.join("./rays", self.mode, str(self.reflexion_steps), f"rays_[{str(idx)}].txt")
             if os.path.exists(ray_path) and not self.force_reflexion_computation:
-                raise Exception("Ray file not found. Please generate ray files before using reflexion channels.")
+                # raise Exception("Debugging raise.")
                 rays = ips.ray_tracing.open(path=ray_path)
             else:
                 # raise Exception("Ray file not found. Please generate ray files before using reflexion channels.")
+                # print("DEBUGGING: DIRECTIONS =", ips.math.get_linear_degree_range(step_size=(self.reflexion_steps/360)))
                 rays = ips.ray_tracing.trace_beams(rel_position=(0.5, 0.5),	
                                                     img_src=np.squeeze(input_img.cpu().numpy(), axis=0),	
-                                                    directions_in_degree=ips.math.get_linear_degree_range(step_size=(self.reflexion_steps/360)*100),	
+                                                    directions_in_degree=ips.math.get_linear_degree_range(start=0, stop=360, step_size=(360/self.reflexion_steps)),	
                                                     wall_values=[0],	
                                                     wall_thickness=0,	
                                                     img_border_also_collide=False,	
                                                     reflexion_order=3,	
                                                     should_scale_rays=True,	
                                                     should_scale_img=False)
+            
+            # DEBUGGING
+            # ips.ray_tracing.print_rays_info(rays)
+            
             # get max value of input image, for ray coloring
             if isinstance(input_img, torch.Tensor):
                 raw_max = input_img.detach().cpu().numpy().max()
@@ -209,11 +217,11 @@ class PhysGenDataset(BaseDataset):
             ray_img = ips.ray_tracing.draw_rays(rays,	
                                                 detail_draw=False,	
                                                 output_format='channels' if self.reflexions_as_channels else 'single_image',	
-                                                img_background=None,	
-                                                ray_value=[int(max_val*0.3), int(max_val*0.5), int(max_val*0.8), int(max_val)],	
+                                                img_background=input_img.detach().permute(1, 2, 0).numpy() if self.reflexions_draw_on_image else None,	
+                                                ray_value=[max_val*0.3, max_val*0.5, max_val*0.8, max_val],	
                                                 ray_thickness=1,	
                                                 img_shape=(height, width),
-                                                should_scale_rays_to_image=True,
+                                                should_scale_rays_to_image=True, # False if self.reflexions_draw_on_image else True,
                                                 show_only_reflections=False)
             # (256, 256)
             ray_img = self.transform(ray_img)
@@ -221,11 +229,15 @@ class PhysGenDataset(BaseDataset):
             if ray_img.ndim == 2:
                 ray_img = ray_img.unsqueeze(0)  # (1, H, W)
 
-            # Merging with input image 
-            if ray_img.shape[1:] == input_img.shape[1:]:
-                input_img = torch.cat((input_img, ray_img), dim=0)
+
+            if self.reflexions_draw_on_image and not self.reflexions_as_channels:
+                input_img = ray_img
             else:
-                raise ValueError(f"Ray image shape {ray_img.shape} does not match input image shape {input_img.shape}.")
+                # Merging with input image 
+                if ray_img.shape[1:] == input_img.shape[1:]:
+                    input_img = torch.cat((input_img, ray_img), dim=0)
+                else:
+                    raise ValueError(f"Ray image shape {ray_img.shape} does not match input image shape {input_img.shape}.")
 
         return input_img, target_img, idx
 

models/pix2pix_model.py
@@ -79,10 +79,12 @@ class Pix2PixModel(BaseModel):
         if is_train:
             parser.set_defaults(pool_size=0, gan_mode='vanilla')
             parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
+            parser.add_argument('--lambda_GAN', type=float, default=1.0, help='weight for L1 loss')
             parser.add_argument('--wgangp', action='store_true', help='Should use WGAN-GP')
             parser.add_argument('--masked', action='store_true', help='Should mask with the target and threshold at 0')
             parser.add_argument('--post_masked', action='store_true', help='Should mask with the target and threshold at 50%')
             parser.add_argument('--use_weighted_loss', action='store_true', help='Should use weighted loss or standard l1-loss.')
+            parser.add_argument('--calc_weight_map_for_weighted_loss', action='store_true', help='Whether to use weight map for the weighted loss.')
             parser.add_argument('--activate_gan_mid_refinement', action='store_true', help='If activated the gan will be purely used as loss at 20 to 80% of the training process.')
         parser.add_argument('--only_reflexions', action='store_true', help='Whether to use only the reflexions as input if using reflexions.')
 
@@ -101,6 +103,8 @@ class Pix2PixModel(BaseModel):
             self.masked = opt.masked
             self.post_masked = opt.post_masked
             self.use_weighted_loss = opt.use_weighted_loss
+            self.calc_weight_map_for_weighted_loss = opt.calc_weight_map_for_weighted_loss
+            self.first_loss_pass = True
             self.train_mask_area = True
 
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
@@ -135,8 +139,8 @@ class Pix2PixModel(BaseModel):
             self.batch_size = opt.batch_size
             self.activate_gan_mid_refinement = opt.activate_gan_mid_refinement
         
-        self.lambda_GAN = 1.0
-        self.lambda_L1 = self.opt.lambda_L1
+            self.lambda_GAN = self.opt.lambda_GAN
+            self.lambda_L1 = self.opt.lambda_L1
         self.epochs_with_gan = 0
         self.forward_passes = 0
         self.current_epoch = 0
@@ -449,16 +453,41 @@ class Pix2PixModel(BaseModel):
         # self.lambda_GAN = 1000.0
 
         # pix2pix reflexion channels
+        # self.weighted_loss = WeightedCombinedLoss( 
+        #                                     silog_lambda=0.5, 
+        #                                     weight_silog=0.0, 
+        #                                     weight_grad=0.0, 
+        #                                     weight_ssim=0.1,
+        #                                     weight_edge_aware=0.0,
+        #                                     weight_l1=1.0,
+        #                                     weight_var=0.01,
+        #                                     weight_range=0.01,
+        #                                     weight_blur=0.1)
+        
+        # self.weighted_loss = WeightedCombinedLoss( 
+        #                                     silog_lambda=0.5, 
+        #                                     weight_silog=0.0, 
+        #                                     weight_grad=1.0, 
+        #                                     weight_ssim=0.3,
+        #                                     weight_edge_aware=0.0,
+        #                                     weight_l1=0.5,
+        #                                     weight_var=0.1,
+        #                                     weight_range=0.00,
+        #                                     weight_blur=0.0)
+
         self.weighted_loss = WeightedCombinedLoss( 
                                             silog_lambda=0.5, 
                                             weight_silog=0.0, 
-                                            weight_grad=0.0, 
-                                            weight_ssim=0.1,
+                                            weight_grad=1.0, 
+                                            weight_ssim=0.3,
                                             weight_edge_aware=0.0,
                                             weight_l1=1.0,
-                                            weight_var=0.01,
-                                            weight_range=0.01,
-                                            weight_blur=0.1)
+                                            weight_var=0.05,
+                                            weight_range=0.0,
+                                            weight_blur=0.0)
+        
+        if hasattr(self, "use_weighted_loss") and not self.use_weighted_loss:
+            self.weighted_loss = None
 
     def set_input(self, input):
         """Unpack input data from the dataloader and perform necessary pre-processing steps.
@@ -539,7 +568,7 @@ class Pix2PixModel(BaseModel):
         self.fake_B = self.netG(self.real_A)  # G(A)
         if self.opt.dataset_mode.lower() == "physgen":
             self.image_names_dict['fake_B'] = self.fake_B if len(self.fake_B.shape) == 4 else self.fake_B.unsqueeze(0)
-        return self.fake_B
+        return self.fake_B.detach()
 
     def backward_D(self):
         """Calculate GAN loss for the discriminator"""
@@ -577,6 +606,11 @@ class Pix2PixModel(BaseModel):
             self.loss_D_gp = 0.0
             self.loss_D = 0.0
 
+        try:
+            self.loss_D = self.loss_D.detach()
+        except Exception:
+            pass
+
     def backward_G(self):
         """Calculate GAN and L1 loss for the generator"""
         # First, G(A) should fake the discriminator
@@ -600,13 +634,19 @@ class Pix2PixModel(BaseModel):
                 self.loss_G_L1 = masked_l1_loss(self.fake_B, self.real_B, self.mask)
         else:
             if self.weighted_loss:
-                self.loss_G_L1 = self.weighted_loss(pred=self.fake_B, target=self.real_B, weight_map=torch.ones_like(self.real_B))
+                if self.calc_weight_map_for_weighted_loss:
+                    mask = None
+                else:
+                    mask = torch.ones_like(self.real_B)
+                self.loss_G_L1 = self.weighted_loss(pred=self.fake_B, target=self.real_B, weight_map=mask, data_idx=None, should_save=False, first_pass=self.first_loss_pass)
+                self.first_loss_pass = False
             else:
                 self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B)
         
         # combine loss and calculate gradients
         self.loss_G = self.loss_G_GAN * self.lambda_GAN + self.loss_G_L1 * self.lambda_L1
         self.loss_G.backward()
+        self.loss_G = self.loss_G.detach()
 
     def optimize_parameters(self):
         self.forward()                   # compute fake images: G(A)